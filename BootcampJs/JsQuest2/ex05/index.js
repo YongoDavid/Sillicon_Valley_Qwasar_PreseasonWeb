6e102161afc820a6ad756a09f7a2b26652cf398c
Yongo David <davidyongo9@gmail.com>
2024-05-28 02:25:44 +0100

    files update

---
+++ BootcampJs/JsQuest2/ex05/index.js
@@ -0,0 +1,13 @@
+        a = 10;
+        b = 9;
+        c = 11;
+        d = 10;
+        y = 9;
+        z = 11;
+
+        if(a) {
+            console.log('a id bigger than b AND smaller than c AND equal to d');
+        }
+        if(z || y){
+            console.log('z OR y are bigger than a')
+        }
